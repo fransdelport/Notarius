36fad31d123acc06d233c9d351e1cc1a24b37890
fransdelport <75753824+fransdelport@users.noreply.github.com>
2020-12-10 16:13:45 -0500

    Add files via upload

---
+++ Notarius.API/Patient.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Notarius.DataAccess;
+using Notarius.Shared.DataModel;
+using Notarius.Shared.DTO;
+
+namespace Notarius.API
+{
+    public static class PatientsAPI
+    {
+
+        public static async Task<ObservableCollection<PatientDTO>> GetPatientsAsync(bool deleted = false)
+        {
+            ObservableCollection<Patient> pats = await PatientData.GetPatientsAsync();
+
+            ObservableCollection<PatientDTO> returnList = new ObservableCollection<PatientDTO>();
+
+            foreach(Patient pat in pats)
+            {
+                PatientDTO pdto = TransformToDTO(pat);
+                returnList.Add(pdto);
+            }
+            return returnList;
+        }
+
+        public static PatientDTO TransformToDTO(Patient pat)
+        {
+            PatientDTO pdto = new PatientDTO();
+            pdto.MRN = pat.MRN;
+            pdto.Firstname = pat.Firstname;
+            pdto.Lastname = pat.Lastname;
+            pdto.Address = pat.Address;
+            pdto.City = pat.City;
+            pdto.State = pat.State;
+            pdto.Zip = pat.Zip;
+            return pdto;
+        }
+
+        public static PatientDTO GetPatientAsync(string mrn)
+        {
+            Patient pat = PatientData.GetPatientAsync(mrn);
+            if (pat == null)
+                return null;
+            PatientDTO pdto = TransformToDTO(pat);
+
+            return pdto;
+        }
+
+        public static async Task<bool> SaveAsync(PatientDTO entity)
+        {
+            Patient pat = TransformFromDTO(entity);
+            return await PatientData.SavePatientAsync(pat);
+
+        }
+
+        private static Patient TransformFromDTO(PatientDTO entity)
+        {
+            Patient pat = new Patient();
+            pat.MRN = entity.MRN;
+            pat.Firstname = entity.Firstname;
+            pat.Lastname = entity.Lastname;
+            pat.Address = entity.Address;
+            pat.City = entity.City;
+            pat.State = entity.State;
+            pat.Zip = entity.Zip;
+            return pat;
+        }
+
+        public static async Task<bool> DeletePatientAsync(string MRN)
+        {
+           return  await PatientData.DeletePatientAsync(MRN);
+
+        }
+    }
+}
---
+++ Notarius.API/Schedule.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using Notarius.DataAccess;
+using Notarius.Shared.DataModel;
+using Notarius.Shared.DTO;
+
+namespace Notarius.API
+{
+    public static class ScheduleAPI
+    {
+        public static async Task<ObservableCollection<ScheduleDTO>> GetAllAsync(bool deleted = false)
+        {
+            ObservableCollection<Schedule> Scheds = await ScheduleData.GetSchedulesAsync();
+
+            ObservableCollection<ScheduleDTO> returnList = new ObservableCollection<ScheduleDTO>();
+
+            foreach (Schedule sched in Scheds)
+            {
+                ScheduleDTO pdto = TransformToDTO(sched);
+                returnList.Add(pdto);
+            }
+            return returnList;
+        }
+
+        public static ScheduleDTO GetAsync(int key)
+        {
+            Schedule sched = ScheduleData.Get(key);
+            if (sched == null)
+                return null;
+            ScheduleDTO dto = TransformToDTO(sched);
+
+            return dto;
+        }
+        private static ScheduleDTO TransformToDTO(Schedule sched)
+        {
+            ScheduleDTO sdto = new ScheduleDTO();
+            sdto.MRN = sched.MRN;
+            sdto.ProviderId = sched.ProviderId;
+            sdto.Patient = PatientsAPI.TransformToDTO(sched.Patient);
+            sdto.ScheduleTime = sched.ScheduleTime;
+
+            return sdto;
+        }
+
+        public static async Task<bool> SaveAsync(ScheduleDTO entity)
+        {
+            Schedule sched = TransformFromDTO(entity);
+            return await ScheduleData.SaveAsync(sched);
+
+        }
+
+        private static Schedule TransformFromDTO(ScheduleDTO dto)
+        {
+            Schedule sched = new Schedule();
+            sched.Key = dto.Key;
+            sched.MRN = dto.MRN;
+            sched.ProviderId = dto.ProviderId;
+            sched.ScheduleTime = dto.ScheduleTime;
+            return sched;
+        }
+        public static async Task<bool> DeleteAsync(int key)
+        {
+            return await ScheduleData.DeleteAsync(key);
+
+        }
+    }
+}
